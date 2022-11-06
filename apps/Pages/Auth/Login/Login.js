4cdedc55d066b130cbfc4f3fd4794d4429d1d525
nanimpacto <nanang@impacto.id>
2022-11-06 19:27:16 +0700

    upload

--- apps/Pages/Auth/Login/Login.js
+++ apps/Pages/Auth/Login/Login.js
@@ -4,7 +4,6 @@ import styles from './LoginStyle';
 import { connect } from 'react-redux';
 import { ADD_USER, DEL_USER } from '../../../Redux/actions/action';
 import RestApi from '../../../Rest/RestApi';
-// import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
 
 class Login extends Component {
     constructor(props) {
@@ -18,37 +17,7 @@ class Login extends Component {
 
     componentDidMount() {
         this.props.delete_user();
-        // GoogleSignin.configure({
-        //     scopes: ['email'],
-        //     // scopes: ['https://www.googleapis.com/auth/userinfo.profile'],
-        //     offlineAccess: true,
-            
-        //     webClientId: "420199211686-75ouh236u16kvfmjofcu2vvsu1tkb7d4.apps.googleusercontent.com",
-        //     iosClientId:"420199211686-75ouh236u16kvfmjofcu2vvsu1tkb7d4.apps.googleusercontent.com"
-        // })
     }
-
-
-    // signInGoogle  = async () => {
-    //     try {
-    //         await GoogleSignin.hasPlayServices();
-    //         const userInfo = await GoogleSignin.signIn();
-    //         console.log(userInfo);
-    //         this.setState({ userInfo });
-    //     } catch (error) {
-    //         console.log(error)
-    //         if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-    //             // user cancelled the login flow
-    //         } else if (error.code === statusCodes.IN_PROGRESS) {
-    //             // operation (e.g. sign in) is in progress already
-    //         } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-    //             // play services not available or outdated
-    //         } else {
-    //             // some other error happened
-    //         }
-    //     }
-    // }
-
 
     onLogin = () => {
         const { user, pass } = this.state;
@@ -106,16 +75,6 @@ class Login extends Component {
                     <TouchableOpacity style={styles.btn_outline} onPress={() => this.props.navigation.push('Register')} activeOpacity={.7}>
                         <Text style={styles.tx_out}>Register</Text>
                     </TouchableOpacity>
-
-                    {/* <TouchableOpacity
-                        onPress={() => this.signInGoogle()}
-                        activeOpacity={.9}
-                        style={styles.btn_google}>
-                        <View style={{ flexDirection: 'row' }}>
-                            <Image resizeMethod="resize" source={require('../../../../assets/icon/google.png')} style={styles.icon_img} />
-                            <Text style={styles.tx_primary}>Sign In Google</Text>
-                        </View>
-                    </TouchableOpacity> */}
                 </View>
             </SafeAreaView>
         );
